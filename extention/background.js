4662e99fc2f67a1a757b67f07089bbd04f6814b8
Angga Priyatna <anggaprytna@gmail.com>
2025-07-11 19:22:22 +0700

    🚑 fix: update blockedWords array in chrome.storage.local.set() in background.js

--- extention/background.js
+++ extention/background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.local.set({
     isEnabled: true,
-    blockedWords: ["shope.ee", "s.shopee.co.id"],
+    blockedWords: ["shope.ee", "s.shopee.co.id", "spf.shopee.co.id"],
   });
 });
